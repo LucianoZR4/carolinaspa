b864639aaff6394b315b74d6b167e5d82631ab42
LucianoZR4 <101479958+LucianoZR4@users.noreply.github.com>
2022-04-09 14:27:59 -0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,10 @@
+window.swiper = new Swiper({
+    el: '.slider__contenido',
+    slideClass: 'slider__slide',
+    createElements: true,
+    autoplay:{
+        delay: 5000
+    },
+    loop: true,
+    navigation: true, 
+})
